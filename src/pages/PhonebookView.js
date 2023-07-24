d09741e12b1b913453849906218cc363c12d5956
AndriiZlt <andreyzlot@gmail.com>
2023-07-24 13:43:11 +0300

    4

--- src/pages/PhonebookView.js
+++ src/pages/PhonebookView.js
@@ -2,10 +2,11 @@ import ContactForm from '../components/ContactForm/ContactForm';
 import ContactList from '../components/Contacts/Contacts';
 import Filter from 'components/Filter/Filter';
 import Container from '@mui/material/Container';
+import css from './PhonebookView.module.css';
 
 const PhonebookView = () => {
   return (
-    <Container component="main" style={{ display: 'flex' }}>
+    <Container component="main" className={css.container}>
       <ContactForm />
       <Container>
         <Filter />
